911cb8750dcf268941fde29a94d97ae0262bedc4
Mahesh Ahirware <maheshahirwar204@gmail.com>
2024-09-04 17:34:51 +0530

    Templates in details.

--- angular-learning/src/app/my-component/my-component.component.ts
+++ angular-learning/src/app/my-component/my-component.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-component',
+
+  // Inline Templates
+  // template:`
+  //           <p>
+  //             Hello {{ courseName }}
+  //           </p>
+  //         `,
+
+  // External Templates 
   templateUrl: './my-component.component.html',
   styleUrl: './my-component.component.css'
 })
 export class MyComponentComponent {
+  courseName:string = "Angular"
+  num:number = 8
+  username:string="";
+  changeName(){
+    this.courseName = "Typescript";
+  }
 
+  showUser(user:string){
+    this.username = user;
+  }
 }
